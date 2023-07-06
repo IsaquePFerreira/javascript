696475ab2b7f5882ac5b84b341a0893193776012
IsaquePFerreira <pferreiraisaque4@gmail.com>
2023-07-06 16:47:02 -0300

    Create AgeVerification project

--- projects/AgeVerification/js/index.js
+++ projects/AgeVerification/js/index.js
@@ -7,6 +7,40 @@ function verify() {
   if (txtYear.value.length == 0 || txtYear.value > year) {
     alert('[ERRO] Verifique os dados e tente novamente!')
   } else {
-    alert('Tudo ok!')
+    let formSex = document.getElementsByName('radsex') 
+    let gender = ''
+    let age = year - Number(txtYear.value)
+    let img = document.createElement('img')
+    let msg = document.createElement('p')
+    msg.setAttribute('id', 'msg')
+    img.setAttribute('id', 'photo')
+
+    if (formSex[0].checked) {
+      gender = 'Homem'
+      if (age >= 0 && age < 10) {
+        img.setAttribute('src', './img/boy.png')
+      } else if (age < 21) {
+        img.setAttribute('src', './img/young-man.png')
+      } else if (age < 50) {
+        img.setAttribute('src', './img/man.png')
+      } else {
+        img.setAttribute('src', './img/old-man.png')
+      }
+    } else if (formSex[1].checked) {
+      gender = 'Mulher'
+      if (age >= 0 && age < 10) {
+       img.setAttribute('src', './img/girl.png') 
+      } else if (age < 21) {
+        img.setAttribute('src', './img/young-woman.png')
+      } else if (age < 50) {
+        img.setAttribute('src', './img/woman.png')
+      } else {
+        img.setAttribute('src', './img/old-woman.png') 
+      }
+    }
+
+    msg.textContent = `Dectamos ${gender} com ${age} anos.`
+    res.insertAdjacentElement('beforeend', msg)
+    res.insertAdjacentElement('beforeend', img)
   }
 }
